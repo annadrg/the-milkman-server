873b507669a3a5657a39390e3c0fe60c704b9c93
annadrg <annaderuig@gmail.com>
2021-04-21 18:11:34 +0200

    Change client url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const options = {
   cors: {
-    origin: "https://milkmansnake.netlify.app/",
+    origin: "https://milkmansnake.netlify.app",
     methods: ["GET", "POST"],
   },
 };
